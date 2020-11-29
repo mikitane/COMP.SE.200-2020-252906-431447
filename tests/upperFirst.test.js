aaaece574c72325dccdfc92a5922a651e369acac
mikitane <miika.luiro@etasku.fi>
2020-11-29 17:25:25 +0200

    Clean up smoke tests

--- tests/upperFirst.test.js
+++ tests/upperFirst.test.js
@@ -4,7 +4,7 @@ import upperFirst from '../src/upperFirst.js';
 const { expect } = chai;
 
 describe('upperFirst', function () {
-  it('All lower case', function () {
+  it('With all lower case', function () {
     const modifiedString = upperFirst('test');
     expect(modifiedString).to.be.equal('Test');
   });
