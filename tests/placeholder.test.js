7191802d69154babd0558d2e0f0e864babf354cb
mikitane <miika.luiro@etasku.fi>
2020-11-29 12:04:20 +0200

    Initial setup for Coveralls

--- tests/placeholder.test.js
+++ tests/placeholder.test.js
@@ -1,1 +1,12 @@
-console.log('Executing placeholder.test.js');
+import chai from 'chai';
+const { expect } = chai;
+
+import myTest from '../src/myTest.js';
+
+
+describe('Placeholder Test', function() {
+    it('Test1', function() {
+        myTest();
+        expect('123').to.be.a('string')
+    });
+})
