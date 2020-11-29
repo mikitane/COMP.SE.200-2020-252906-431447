4f8f79afb3e25f25449ed90baefa89f379a533b4
mikitane <miika.luiro@etasku.fi>
2020-11-29 16:40:10 +0200

    Smoke test: memoize

---
+++ tests/memoize.test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import memoize from '../src/memoize.js';
+
+const { expect } = chai;
+
+describe('memoize', function () {
+  it('Without proper resolver', function () {
+    const obj = { 'a': 2 };
+    const func = memoize(x => x.a * 2);
+    const origValue = func(obj);
+
+    // memoized function does not notice a changed
+    // value inside an object. So should return the
+    // memoized value.
+    obj.a = 3;
+    const memoizedValue = func(obj);
+
+    expect(origValue).to.be.equal(4);
+    expect(memoizedValue).to.be.equal(4);
+  });
+});
