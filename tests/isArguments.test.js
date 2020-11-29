993f11dc41d36d9b3b6c960f2d0f1be4fe3230ea
mikitane <miika.luiro@etasku.fi>
2020-11-29 17:00:56 +0200

    Smoke test: isArguments

---
+++ tests/isArguments.test.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+import isArguments from '../src/isArguments.js';
+
+const { expect } = chai;
+
+describe('isArguments', function () {
+  it('With function returning arguments', function () {
+    const _isArguments = isArguments(function() { return arguments }())
+    expect(_isArguments).to.be.true;
+  });
+
+  it('With array of integers', function () {
+    const _isArguments = isArguments([1, 2])
+    expect(_isArguments).to.be.false;
+  });
+});
