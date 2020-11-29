462002e912eef51e53513c4ea589c81e684716c3
mikitane <miika.luiro@etasku.fi>
2020-11-29 13:14:34 +0200

    Smoke test: words

--- tests/words.test.js
+++ tests/words.test.js
@@ -8,4 +8,9 @@ describe('Words', function () {
     const wordsArray = words('');
     expect(wordsArray).to.be.an('array').that.is.empty;
   });
+
+  it('Two words', function() {
+    const wordsArray = words('My Test');
+    expect(wordsArray).to.be.an('array').that.have.lengthOf(2);
+  });
 });
