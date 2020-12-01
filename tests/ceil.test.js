624ecd72b784e9b53fd28f15b3bc91332c157a7f
mikitane <miika.luiro@etasku.fi>
2020-12-01 21:29:37 +0200

    Common e-commerce use-case tests: ceil

--- tests/ceil.test.js
+++ tests/ceil.test.js
@@ -20,4 +20,12 @@ describe("suurempi tai yhtasuuri", () =>{
    it("toinen parametri = -1", () =>{
       expect(ceil(1.1, -2)).to.deep.equal(10)
    });
+
+   it("Number with precision of three rounded up to precision of two", function() {
+      expect(ceil(10.111, 2)).to.be.equal(10.12);
+   });
+
+   it("Number with precision of two rounded up to precision of two", function() {
+      expect(ceil(10.11, 2)).to.be.equal(10.11);
+   });
 })
