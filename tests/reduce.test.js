5cc43ac98cff5d850d1376fbbaca1e7ab5bab98d
mikitane <miika.luiro@etasku.fi>
2020-12-01 20:54:53 +0200

    Common e-commerce use-case tests: reduce

--- tests/reduce.test.js
+++ tests/reduce.test.js
@@ -4,8 +4,24 @@ import reduce from '../src/reduce.js';
 const { expect } = chai;
 
 describe('reduce', function () {
-  it('With list of integers', function () {
+  it('With array of integers', function () {
     const reducedValue = reduce([1, 2, 3], (sum, n) => sum + n, 0);
     expect(reducedValue).to.be.equal(6);
+  });
+
+  it('With array of products, count total price', function () {
+    const products = [
+      { name: 'My Product 1', price: 10.24 },
+      { name: 'My Product 2', price: 2.51 },
+      { name: 'My Product 3', price: 200.52 },
+    ];
+
+    const totalPrice = reduce(products, (sum, product) => sum + product.price, 0);
+    expect(totalPrice).to.be.equal(213.27);
+  });
+
+  it('With empty array', function () {
+    const reducedValue = reduce([], (sum, n) => sum + n, 0);
+    expect(reducedValue).to.be.equal(0);
   });
 });
