4aab88af627f1d68baa5d2e8da21ee332d2353f2
mikitane <miika.luiro@etasku.fi>
2020-12-01 21:17:30 +0200

    Common e-commerce use-case tests: countBy

--- tests/countBy.test.js
+++ tests/countBy.test.js
@@ -16,4 +16,15 @@ describe("Laskuri", () =>{
    it("tyhjä", () =>{
       expect(countBy([{'a': 1, 'b': 1}, {'a': 2, 'b': 2}], x => x.c)).to.include({})
    });
+
+   it("With array of products, count number of same product", function() {
+      const products = [
+         { name: 'My Product 1', price: 10.24 },
+         { name: 'My Product 2', price: 2.51 },
+         { name: 'My Product 3', price: null },
+         { name: 'My Product 1', price: 10.24 },
+       ];
+
+       expect(countBy(products, product => product.name)).to.deep.equal({'My Product 1': 2, 'My Product 2': 1, 'My Product 3': 1})
+   })
 })
