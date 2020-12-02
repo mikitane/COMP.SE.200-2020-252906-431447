1d70d70c7481401b922f61e53317fb670bdc4e42
mikitane <miika.luiro@etasku.fi>
2020-12-02 18:13:25 +0200

    More tests for isEmpty (100% branch coverage)

--- tests/isEmpty.test.js
+++ tests/isEmpty.test.js
@@ -23,4 +23,62 @@ describe('isEmpty', function () {
     const _isEmpty = isEmpty([{ product: 'My Product 1', price: 10.20 }]);
     expect(_isEmpty).to.be.false;
   });
+
+  it('With null', function () {
+    const _isEmpty = isEmpty(null);
+    expect(_isEmpty).to.be.true;
+  });
+
+  it('With empty object', function () {
+    const _isEmpty = isEmpty({});
+    expect(_isEmpty).to.be.true;
+  });
+
+  it('With filled object', function () {
+    const _isEmpty = isEmpty({a:1});
+    expect(_isEmpty).to.be.false;
+  });
+
+  it('With empty string', function () {
+    const _isEmpty = isEmpty('');
+    expect(_isEmpty).to.be.true;
+  });
+
+  it('With filled string', function () {
+    const _isEmpty = isEmpty('test');
+    expect(_isEmpty).to.be.false;
+  });
+
+  it('With empty arguments', function () {
+    const _isEmpty = isEmpty(function() { return arguments }());
+    expect(_isEmpty).to.be.true;
+  });
+
+  it('With filled arguments', function () {
+    const _isEmpty = isEmpty(function(x) { return arguments }(1));
+    expect(_isEmpty).to.be.false;
+  });
+
+  it('With empty Map', function () {
+    const _isEmpty = isEmpty(new Map());
+    expect(_isEmpty).to.be.true;
+  });
+
+  it('With filled Map', function () {
+    const _isEmpty = isEmpty(new Map([['a', 1]]));
+    expect(_isEmpty).to.be.false;
+  });
+
+  it('With object created with new keyword (empty prototype)', function () {
+    function func() {}
+    const _isEmpty = isEmpty((new func()).constructor.prototype);
+    expect(_isEmpty).to.be.true;
+  });
+
+  it('With object created with new keyword (filled prototype)', function () {
+    function func() {}
+    func.prototype.test = 1;
+    const _isEmpty = isEmpty((new func()).constructor.prototype);
+    expect(_isEmpty).to.be.false;
+  });
 });
--- tests/toString.test.js
+++ tests/toString.test.js
@@ -3,10 +3,8 @@ import toString from '../src/toString.js';
 
 const { expect } = chai;
 
-// Fails
 describe('toString', function () {
   it('With null', function () {
-    this.skip(); // Skip temporarily to make tests pass
     const string = toString(null)
     expect(string).to.be.equal('');
   });
