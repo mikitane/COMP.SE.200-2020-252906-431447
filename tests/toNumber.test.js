c1b7d9976cfff766f5cdaa89d2109b6ad5aa3831
mikitane <miika.luiro@etasku.fi>
2020-12-02 18:59:40 +0200

    More tests for toNumber (100% branch coverage)

--- tests/toNumber.test.js
+++ tests/toNumber.test.js
@@ -8,4 +8,49 @@ describe('toNumber', function () {
     const number = toNumber('1.1')
     expect(number).to.be.equal(1.1);
   });
+
+  it('With Number', function () {
+    const number = toNumber(1.1)
+    expect(number).to.deep.equal(1.1);
+  });
+
+  it('With Symbol', function () {
+    const number = toNumber(Symbol())
+    expect(number).to.deep.equal(NaN);
+  });
+
+  it('With empty Object', function () {
+    const number = toNumber({})
+    expect(number).to.deep.equal(NaN);
+  });
+
+  it('With valueOf function in object returning 1 ', function () {
+    const number = toNumber({ valueOf: () => 1 })
+    expect(number).to.equal(1);
+  });
+
+  it('With valueOf function in object returning 0 ', function () {
+    const number = toNumber({ valueOf: () => 0 })
+    expect(number).to.equal(0);
+  });
+
+  it('With valueOf property in object ', function () {
+    const number = toNumber({ valueOf: 1 })
+    expect(number).to.deep.equal(NaN);
+  });
+
+  it('With binary representing 1', function () {
+    const number = toNumber('0b0001')
+    expect(number).to.equal(1);
+  });
+
+  it('With octal representing 1', function () {
+    const number = toNumber('0o1')
+    expect(number).to.equal(1);
+  });
+
+  it('With bad signed hex', function () {
+    const number = toNumber('-0x0001')
+    expect(number).to.equal(NaN);
+  });
 });
