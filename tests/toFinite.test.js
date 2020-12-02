89e580b034ba09f8f44be95d3b6285ce8ae6d43e
mikitane <miika.luiro@etasku.fi>
2020-12-02 20:54:49 +0200

    More tests for toFinite (91,67% branch coverage)

--- tests/toFinite.test.js
+++ tests/toFinite.test.js
@@ -8,4 +8,24 @@ describe('toFinite', function () {
     const finiteNumber = toFinite(Infinity)
     expect(finiteNumber).to.be.equal(1.7976931348623157e+308);
   });
+
+  it('With -Infinity', function () {
+    const finiteNumber = toFinite(-Infinity)
+    expect(finiteNumber).to.be.equal(-1.7976931348623157e+308);
+  });
+
+  it('With null', function () {
+    const finiteNumber = toFinite(null)
+    expect(finiteNumber).to.be.equal(0);
+  });
+
+  it('With zero', function () {
+    const finiteNumber = toFinite(0)
+    expect(finiteNumber).to.be.equal(0);
+  });
+
+  it('With one', function () {
+    const finiteNumber = toFinite(1)
+    expect(finiteNumber).to.be.equal(1);
+  });
 });
