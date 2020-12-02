17aaffe534f1a60c4021e2780603d9017f7efa98
mikitane <miika.luiro@etasku.fi>
2020-12-02 18:26:04 +0200

    More tests for slice (100% branch coverage)

--- tests/slice.test.js
+++ tests/slice.test.js
@@ -8,4 +8,29 @@ describe('slice', function () {
     const _slice = slice([1, 2, 3, 4, 5], 1, 3);
     expect(_slice).to.have.members([2, 3]);
   });
+
+  it('With start and end params not set', function () {
+    const _slice = slice([1, 2, 3]);
+    expect(_slice).to.have.members([1, 2, 3]);
+  });
+
+  it('With empty array, no start and end params', function () {
+    const _slice = slice([]);
+    expect(_slice).to.be.empty;
+  });
+
+  it('With null', function () {
+    const _slice = slice(null);
+    expect(_slice).to.be.empty;
+  });
+
+  it('With negative start and end params (smaller than array length)', function () {
+    const _slice = slice([1, 2, 3, 4, 5], -3, -1);
+    expect(_slice).to.have.members([3, 4]);
+  });
+
+  it('With negative start and end params (bigger than array length)', function () {
+    const _slice = slice([1, 2, 3, 4, 5], -10, -15);
+    expect(_slice).to.be.empty;
+  });
 });
