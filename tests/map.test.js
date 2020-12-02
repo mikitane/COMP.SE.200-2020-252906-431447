a13b72b6a3468ddafbe8974d857c463c27191966
mikitane <miika.luiro@etasku.fi>
2020-12-02 19:36:47 +0200

    More tests for map (100% branch coverage)

--- tests/map.test.js
+++ tests/map.test.js
@@ -8,4 +8,9 @@ describe('map', function () {
     const mappedValues = map([1, 2, 3], x => x * 2);
     expect(mappedValues).to.have.members([2, 4, 6]);
   });
+
+  it('With null as first parameter', function () {
+    const mappedValues = map(null, x => x);
+    expect(mappedValues).to.be.empty;
+  });
 });
