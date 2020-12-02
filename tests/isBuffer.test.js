735765a5672a8c9dd9cc56c1a2dd57547d1340f2
mikitane <miika.luiro@etasku.fi>
2020-12-02 18:13:25 +0200

    More tests for isEmpty (100% branch coverage)

--- tests/isBuffer.test.js
+++ tests/isBuffer.test.js
@@ -5,9 +5,7 @@ const { expect } = chai;
 
 describe('isBuffer', function () {
 
-  // Fails
   it('With Buffer', function () {
-    this.skip(); // Skip temporarily to make tests pass
     const _isBuffer = isBuffer(new Buffer(2))
     expect(_isBuffer).to.be.true;
   });
